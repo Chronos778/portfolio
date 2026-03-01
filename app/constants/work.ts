2596622f5524ec9662591a80ca01e7c4daa6beca
Chronos778 <god669213@gmail.com>
2026-03-01 20:15:04 +0530

    .

--- app/constants/work.ts
+++ app/constants/work.ts
@@ -6,7 +6,7 @@ export const WORK_TIMELINE: WorkTimelinePoint[] = [
     point: new THREE.Vector3(0, 0, 0),
     year: '2024',
     title: 'B.Tech CSE (Data Science)',
-    subtitle: '',
+    subtitle: 'SJCEM',
     position: 'right',
   },
   {
